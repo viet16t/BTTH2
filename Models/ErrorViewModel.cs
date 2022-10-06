1211d169a2f76b67463011e4f36e233ae2f90f32
Việt Quốc <you@example.com>
2022-10-06 08:13:28 +0700

    BTTH 2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DoanQuocVietBTTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
